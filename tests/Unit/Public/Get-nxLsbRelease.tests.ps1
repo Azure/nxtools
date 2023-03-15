44866490931ff67993997c9566f21c6e334c4074
gaelcolas <gaelcolas@synedgy.com>
2023-03-15 16:40:14 +0100

    updating lsb release test

--- tests/Unit/Public/Get-nxLsbRelease.tests.ps1
+++ tests/Unit/Public/Get-nxLsbRelease.tests.ps1
@@ -1,5 +1,5 @@
 Describe 'Get-LsbRelease function' -skip:($IsWindows -or $IsMacOS) {
     it 'should not throw (if the binary is installed)' {
-     {Get-LsbRelease} | Should -not -Throw
+     {Get-nxLsbRelease} | Should -not -Throw
     }
 }
