7d8900dbbcc78b81b61b8a81967306f21b999da5
Gurraj Atwal <gurrajatwal@microsoft.com>
2023-10-10 15:37:38 -0700

    Add mocks to Get-nxLsbRelease unit test

--- tests/Unit/Public/Get-nxLsbRelease.tests.ps1
+++ tests/Unit/Public/Get-nxLsbRelease.tests.ps1
@@ -1,5 +1,25 @@
-Describe 'Get-LsbRelease function' -skip:($IsWindows -or $IsMacOS) {
-    it 'should not throw (if the binary is installed)' {
-     {Get-nxLsbRelease} | Should -not -Throw
+Describe 'Get-LsbRelease function' {
+    Context "When the lsb-release package is installed" {
+        BeforeAll {
+            Mock -ModuleName 'nxtools' -CommandName 'Invoke-NativeCommand' -ParameterFilter {
+                return $Executable -eq "lsb_release"
+            } -MockWith {
+                return @(
+                    "No LSB modules are available.",
+                    "Distributor ID: Ubuntu",
+                    "Description:    Ubuntu 20.04.6 LTS",
+                    "Release:        20.04",
+                    "Codename:       focal"
+                )
+            }
+        }
+
+        It 'Should return information about the machine' {
+            $result = Get-nxLsbRelease
+            $result.DistributorID | Should -Be 'Ubuntu'
+            $result.Description | Should -Be 'Ubuntu 20.04.6 LTS'
+            $result.Release | Should -Be '20.04'
+            $result.Codename | Should -Be 'focal'
+        }
     }
 }
